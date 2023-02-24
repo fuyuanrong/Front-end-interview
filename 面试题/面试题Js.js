2cca43799e65d0a14a60c51d74c0d97140437e53
fuyuanrong <361551604@qq.com>
2023-02-24 16:22:07 +0800

    面试题

--- 面试题/面试题Js.js
+++ 面试题/面试题Js.js
@@ -3,43 +3,103 @@
 // const fn = (value) => {
 // ...
 // }
-fn(112) // 输出 [1， 11， 112]
+fnc(112) // 输出 [1， 11， 112]
 
-function fn(value){
+function fnc(value){
 	let len = value.length;
-	let result = [];
+	let  resultArr : any = [];
 	for (var i = 0; i < len; i++) {
-		result[i]='';
+		resultArr[i] = "";
 	}
 	for (var i = 0; i < value.length; i++) {
 		for (var j = i; j >= 0; j--) {
-			result[i]= value[j] + result[i];
+			resultArr[i]= value[j] + resultArr[i];
 		}
 	}
-	return result
+	return resultArr
 }
 // 某公司 1 到 12 月份的销售额存在一个对象里面如下：
 // {1:222, 2:123, 5:888}，请把数据处理为如下结构：[222, 123, null, null, 888, null, null, null, null, null, null, null]。
 
 let  obj = {1:222, 2:123, 5:888}
-let result = [];
+let resultArr : any= [];
 for (var i = 0; i < 12; i++) {
-	result[i] = null
+	resultArr[i] = null;
 }
 for(let i in  obj){
 	if(obj.hasOwnProperty(i)){
-		result[Number(i)-1] = obj[i]
+		resultArr[Number(i)-1] = obj[i];
 	}
 }
-console.log(result)
+console.log(resultArr)
 
 // 实现一个函数 5.add(3).minus(2)功能。例：5 + 3 - 2，结果为 6
 
-Number.prototype.add = function(v) {
-	v =  v?Number(v):0
-	return this + v
-};
-Number.prototype.minus = function(v){
-	v =  v?Number(v):0
-	return this - v
-}
+// Number.prototype.add = function(v) {
+// 	v =  v?Number(v):0
+// 	return this + v
+// };
+// Number.prototype.minus = function(v){
+// 	v =  v?Number(v):0
+// 	return this - v
+// }
+
+
+
+// 美团面试题
+	function fn(){
+		return new Promise((resolve)=>{
+			console.log('promise1');
+			fn1();
+			setTimeout(() => {
+				console.log('promise2');
+				resolve('');
+				console.log('promise3');
+			}, 0);
+		})
+	}
+	async function fn1(){
+		var p = Promise.resolve().then(()=>{
+			console.log('promise6');
+		})
+		await p.then(()=>{
+			console.log('promise7');
+		})
+		console.log('end');
+	}
+	console.log('script');
+	setTimeout(() => {
+		console.log('setTimeout');
+	}, 0);
+	fn().then(()=>{
+		console.log('promise4');
+	})
+
+	// 结果
+
+	/*
+		script
+		promise1
+
+		**fn1**
+		promise6
+		promise7
+		end
+
+		setTimeout
+		promise2
+		promise3
+		promise4
+
+		解析
+		异步
+		宏
+		setTimeout
+		promise2
+		promise3
+
+		微
+		resolve('')
+	
+	
+	*/
