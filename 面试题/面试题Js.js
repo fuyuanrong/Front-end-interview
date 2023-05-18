5be4a13158b464652624fb0f22e034583a3c61b7
Aron <44282927+fuyuanrong@users.noreply.github.com>
2023-05-18 11:45:45 +0800

    111

--- 面试题/面试题Js.js
+++ 面试题/面试题Js.js
@@ -22,7 +22,7 @@ function fnc(value){
 // 某公司 1 到 12 月份的销售额存在一个对象里面如下：
 // {1:222, 2:123, 5:888}，请把数据处理为如下结构：[222, 123, null, null, 888, null, null, null, null, null, null, null]。
 
-let  obj = {1:222, 2:123, 5:888}
+var  obj = {1:222, 2:123, 5:888}
 let resultArr = [];
 for (var i = 0; i < 12; i++) {
 	resultArr[i] = null;
